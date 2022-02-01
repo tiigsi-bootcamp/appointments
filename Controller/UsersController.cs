5f45624a1347034cc345cc905c50d77aa64b39a5
Mohamed Ali <somcoder@outlook.com>
2022-02-01 19:09:56 +0300

    Implemented user update and delete endpoints

--- Controller/UsersController.cs
+++ Controller/UsersController.cs
@@ -19,7 +19,7 @@ public class UsersController : ControllerBase
 	[HttpGet]
 	public IActionResult Get()
 	{
-		var users = _context.Users.ToList();
+		var users = _context.Users.OrderByDescending(u => u.FullName).ToList();
 		return Ok(users);
 	}
 
@@ -44,5 +44,42 @@ public class UsersController : ControllerBase
 		_context.SaveChanges();
 
 		return Created("", user);
+	}
+
+	// PUT /users/5
+	[HttpPut("{id}")]
+	public IActionResult Update(int id, [FromBody]User user)
+	{
+		var targetUser = _context.Users.Find(id);
+		if (targetUser is null)
+		{
+			return BadRequest();
+		}
+
+		targetUser.FullName = user.FullName;
+		targetUser.Address = user.Address;
+		targetUser.Email = user.Email;
+		targetUser.Gender = user.Gender;
+		
+		_context.Users.Update(targetUser);
+		_context.SaveChanges();
+
+		return NoContent();
+	}
+
+	// DELETE /users/5
+	[HttpDelete("{id}")]
+	public IActionResult Delete(int id)
+	{
+		var user = _context.Users.Find(id);
+		if (user is null)
+		{
+			return BadRequest();
+		}
+
+		_context.Users.Remove(user);
+		_context.SaveChanges();
+
+		return NoContent();
 	}
 }
