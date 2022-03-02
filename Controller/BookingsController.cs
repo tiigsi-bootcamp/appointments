9d0642409a5a23decd7d493a393d4781ca2eb605
Mohamed Ali <somcoder@outlook.com>
2022-03-02 18:21:15 +0300

    Added bookings feature

--- Controller/BookingsController.cs
+++ Controller/BookingsController.cs
@@ -2,11 +2,13 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 using Data;
+using Models;
 using ViewModels;
 
 namespace Controllers;
 
 [Route("[controller]")]
+[ApiController]
 public class BookingsController : ControllerBase
 {
 	private readonly AppointmentsDbContext _context;
@@ -26,8 +28,51 @@ public class BookingsController : ControllerBase
 	}
 
 	// POST /bookings
+	[HttpPost]
 	public async Task<IActionResult> Add([FromBody]BookingViewModel viewModel)
 	{
+		var timeSlot = await _context.TimeSlots
+			.Include(ts => ts.Schedule).ThenInclude(s => s.Doctor)
+			.SingleOrDefaultAsync(ts => ts.Id == viewModel.TimeSlotId);
+		if (timeSlot is null)
+		{
+			return BadRequest("Selected time-slot could not be recognized.");
+		}
+
+		if (viewModel.AppointmentTime < DateTime.Today)
+		{
+			return BadRequest("The selected appointment-time cannot be in the past! 😁");
+		}
+
+		if (viewModel.AppointmentTime.DayOfWeek != timeSlot.Schedule.Day)
+		{
+			return BadRequest("Doctor is not available at the selected day!");
+		}
 		
+		var ticketPrice = timeSlot.Schedule.Doctor.TicketPrice;
+		var rate = 0.02m;
+		var commission = ticketPrice * rate;
+
+		// TODO: Add real payment gateway (eDahab, Zaad...).
+
+		var transactionId = new Random().Next(10_000, 999_999);
+		var booking = new Booking
+		{
+			AppointmentTime = new DateTime(viewModel.AppointmentTime.Ticks, DateTimeKind.Utc),
+			IsCompleted = false,
+			UserId = 5, // TODO: Get the userId from session.
+			CreatedAt = DateTime.UtcNow,
+			TransactionId = $"{transactionId}",
+			PaidAmount = ticketPrice,
+			Commission = commission,
+			DoctorRevenue = ticketPrice - commission,
+			PaymentMethod = viewModel.PaymentMethod,
+			TimeSlotId = timeSlot.Id,
+		};
+
+		await _context.Bookings.AddAsync(booking);
+		await _context.SaveChangesAsync();
+
+		return Created("", booking);
 	}
 }
--- ViewModels/BookingViewModel.cs
+++ ViewModels/BookingViewModel.cs
@@ -5,4 +5,6 @@ public class BookingViewModel
 	public DateTime AppointmentTime { get; set; }
 
 	public int TimeSlotId { get; set; }
+
+	public string PaymentMethod { get; set; } = "";
 }
